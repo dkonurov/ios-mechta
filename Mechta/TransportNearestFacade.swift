1f8dd36ed294bb84342371dceecc107b22a0daba
evsafronov <evsafronov.developer@yandex.ru>
2017-02-17 08:42:08 +0300

    Начал работу над моделью ближайших рейсов

--- Mechta/TransportNearestFacade.swift
+++ Mechta/TransportNearestFacade.swift
@@ -21,4 +21,23 @@ class TransportNearestFacade {
     func onRouteChanged() {
         NotificationCenter.default.post(name: TransportNearestFacade.updatedNotification, object: nil)
     }
+    
+    //todo учитывать доступность рейса в будний/выходной
+    //todo добавить расчет времени до начала рейса
+    var nearest: [(startTime: String, endTime: String, left: String)] {
+        guard let start = model.startBusStop else {
+            return []
+        }
+        guard let end = model.endBusStop else {
+            return []
+        }
+        
+        let nearest = model.nearest(from: start, to: end)
+        return nearest.map() { start, end in
+            let startTime = start.stopTime ?? ""
+            let endTime = end.stopTime ?? ""
+            let leftTime = ""
+            return (startTime, endTime, leftTime)
+        }
+    }
 }
