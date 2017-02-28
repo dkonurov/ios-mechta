eaccbe568868a1e575193976d78ceb616815d567
evsafronov <evsafronov.developer@yandex.ru>
2017-02-28 20:57:46 +0300

    Заменил маркеры на карте

--- Mechta/TransportMapViewController.swift
+++ Mechta/TransportMapViewController.swift
@@ -5,6 +5,8 @@ class TransportMapViewController: UIViewController {
     @IBOutlet weak var mapView: YandexMapView!
     
     private let model = TransportMapFacade()
+    private let presetBusStop = "islands#blueMassTransitCircleIcon"
+    private let presetBusStopFirst = "islands#nightMassTransitCircleIcon"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,15 +21,19 @@ class TransportMapViewController: UIViewController {
     
     func updateData() {
         mapView.clear()
+        
         let busStops = model.busStops
+        
         for busStop in busStops {
             mapView.showMarker(id: Int(busStop.id),
                                latitude: busStop.latitude,
                                longitude: busStop.longitude,
-                               title: busStop.title ?? "Остановка")
+                               title: busStop.title ?? "Остановка",
+                               preset: busStop.id == busStops.first?.id ? presetBusStopFirst : presetBusStop)
         }
+        
         if let startBusStop = busStops.first {
-            mapView.setZoom(zoom: 15)
+            mapView.setZoom(zoom: 16)
             mapView.setCenter(latitude: startBusStop.latitude, longitude: startBusStop.longitude)
         }
     }
